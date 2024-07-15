0d4c6028663cb4c6352f5b594e36078efe1c30f3
Joshua Larouche <joshua.larouche@gov.bc.ca>
2024-07-15 08:05:29 -0700

    UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,46 +10,52 @@ import {
   RefinementList 
 } from "react-instantsearch-dom";
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
-import "instantsearch.css/themes/algolia-min.css";
+import 'instantsearch.css/themes/satellite.css';
 import "./App.css";
+import logo from './assets/BCID_H_rgb_pos.png';
 
-// TODO: Protect key in secret 
 const searchClient = instantMeiliSearch(
   "https://test.doc.search.apps.silver.devops.gov.bc.ca",
   "M01OpyNyPSDikZ9o-A7ZqKEbbl4QgrwiipMVW--6Y1Q"
+  // "http://localhost:7700/",
+  // "AcP1KBq-S9mZbDizXcEQ87xvf5kT9FCRyk-k-iXgtkA"
 );
 
-// Mapping long content-type names to user-friendly names
 const contentTypeMapping = {
   "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "Word",
   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "Excel",
   "application/vnd.openxmlformats-officedocument.presentationml.presentation": "PowerPoint",
   "application/pdf": "PDF",
   "application/json; charset=UTF-8": "Plain Text",
-  // Add other mappings as needed
 };
 
 const App = () => (
   <div className="ais-InstantSearch">
-    <h1>Document Search Proof of Concept</h1>
+    <h1>
+      <img src={logo} alt="Logo" className="header-logo" /> {/* Add the logo image */}
+      Document Search Proof of Concept
+    </h1>
     <InstantSearch indexName="test" searchClient={searchClient}>
-      <div className="left-panel">
-        <h2>Filter by Content Type</h2>
-        <RefinementList 
-          attribute="Content-Type" 
-          transformItems={items =>
-            items.map(item => ({
-              ...item,
-              label: contentTypeMapping[item.label] || item.label,
-            }))
-          }
-        />
+        <SearchBox />
+        <hr className="delineating-line" />
+        <div className="filter-options">
+          <h2>Filter by Content Type</h2>
+          <RefinementList 
+            attribute="Content-Type" 
+            transformItems={items =>
+              items.map(item => ({
+                ...item,
+                label: contentTypeMapping[item.label] || item.label,
+              }))
+            }
+          />
       </div>
       <div className="right-panel">
-        <SearchBox />
         <Stats />
         <Configure attributesToSnippet={["X-TIKA:content:100"]} hitsPerPage={10} />
-        <InfiniteHits hitComponent={Hit} />
+        <div className="hits">
+          <InfiniteHits hitComponent={Hit} />
+        </div>
       </div>
     </InstantSearch>
   </div>
@@ -57,30 +63,14 @@ const App = () => (
 
 const Hit = ({ hit }) => (
   <div className="hit" key={hit.id}>
-    <div className="hit-field">
-      <strong>Title:</strong> <Highlight attribute="dc:title" hit={hit} />
-    </div>
-    <div className="hit-field">
-      <strong>Last Author:</strong> <Highlight attribute="meta:last-author" hit={hit} />
-    </div>
-    <div className="hit-field">
-      <strong>Creator:</strong> <Highlight attribute="dc:creator" hit={hit} />
-    </div>
-    <div className="hit-field">
-      <strong>Publisher:</strong> <Highlight attribute="dc:publisher" hit={hit} />
-    </div>
-    <div className="hit-field">
-      <strong>Company:</strong> <Highlight attribute="extended-properties:Company" hit={hit} />
-    </div>
-    <div className="hit-field">
-      <strong>Date Created:</strong> {hit["dcterms:created"]} {/* Display created date */}
-    </div>
-    <div className="hit-field">
-      <strong>Date Last Modified:</strong> {hit["dcterms:modified"]} {/* Display modified date */}
-    </div>
-    <div className="hit-field">
-      <strong>Content:</strong> <Snippet attribute="X-TIKA:content" hit={hit} tagName="mark" />
-    </div>
+    <div className="hit-field"><strong>Title:</strong> <Highlight attribute="dc:title" hit={hit} /></div>
+    <div className="hit-field"><strong>Last Author:</strong> <Highlight attribute="meta:last-author" hit={hit} /></div>
+    <div className="hit-field"><strong>Creator:</strong> <Highlight attribute="dc:creator" hit={hit} /></div>
+    <div className="hit-field"><strong>Publisher:</strong> <Highlight attribute="dc:publisher" hit={hit} /></div>
+    <div className="hit-field"><strong>Company:</strong> <Highlight attribute="extended-properties:Company" hit={hit} /></div>
+    <div className="hit-field"><strong>Date Created:</strong> {hit["dcterms:created"]}</div>
+    <div className="hit-field"><strong>Date Last Modified:</strong> {hit["dcterms:modified"]}</div>
+    <div className="hit-field"><strong>Content:</strong> <Snippet attribute="X-TIKA:content" hit={hit} tagName="mark" /></div>
   </div>
 );
 
