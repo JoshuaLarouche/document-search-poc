55472ef8b8c60d347f02be547ced621606115200
Joshua Larouche <joshua.larouche@gov.bc.ca>
2024-08-07 18:41:08 -0700

    fixed metadata addition

--- src/UploadPage.jsx
+++ src/UploadPage.jsx
@@ -5,9 +5,9 @@ import XHRUpload from "@uppy/xhr-upload";
 import "@uppy/core/dist/style.css";
 import "@uppy/dashboard/dist/style.css";
 import "./UploadPage.css";
-import logo from './assets/BCID_H_rgb_pos.png';
-import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
+import logo from "./assets/BCID_H_rgb_pos.png";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 import { ClipLoader } from "react-spinners"; // Import the spinner component
 
 const UploadPage = () => {
@@ -15,125 +15,140 @@ const UploadPage = () => {
   const [loading, setLoading] = useState(false); // State to manage loading
   const [metadataFields, setMetadataFields] = useState({
     hyperlink: "",
-    additionalField: ""
+    additionalField: "",
   });
 
   useEffect(() => {
+    if (uppyRef.current) {
+      return;
+    }
+
     const uppy = new Uppy({
-      id: 'uppy',
+      id: "uppy",
       autoProceed: false,
       restrictions: {
         maxNumberOfFiles: 1,
-      }
+      },
     })
-    .use(Dashboard, {
-      inline: true,
-      target: '#uppy-dashboard',
-      showProgressDetails: true,
-      height: 400
-    })
-    .use(XHRUpload, {
-      method: 'PUT',
-      endpoint: 'http://localhost:5173/tika/tika', // This needs to match your backend endpoint
-      fieldName: 'file',
-      headers: (file) => ({
-        'Content-Type': file.type,
-        'Accept': 'application/json',
+      .use(Dashboard, {
+        inline: true,
+        target: "#uppy-dashboard",
+        showProgressDetails: true,
+        height: 400,
+        hideUploadButton: true,
       })
-    });
-
-    uppy.on('file-added', (file) => {
-      console.log('File added to Uppy:', file);
-      // Attach metadata to the file
-      uppy.setFileMeta(file.id, {
-        hyperlink: metadataFields.hyperlink,
-        additionalField: metadataFields.additionalField
+      .use(XHRUpload, {
+        method: "PUT",
+        endpoint: "http://localhost:5173/tika/tika", // This needs to match your backend endpoint
+        fieldName: "file",
+        headers: (file) => ({
+          "Content-Type": file.type,
+          Accept: "application/json",
+        }),
       });
+
+    uppy.on("file-added", (file) => {
+      // Attach metadata to the file
+      console.log("File added to Uppy:", file);
+      // uppy.setFileMeta(file.id, {
+      //   hyperlink: metadataFields.hyperlink,
+      //   additionalField: metadataFields.additionalField,
+      // });
     });
 
-    uppy.on('upload', () => {
-      console.log('Upload started');
+    uppy.on("upload", () => {
+      console.log("Upload started");
       setLoading(true); // Show loading when upload starts
     });
 
-    uppy.on('upload-success', (file, response) => {
-      console.log('Upload successful:', response);
-
-      // Use response to get the uploaded file data and proceed with the next steps
-      const fileType = file.type;
-      console.log('File type:', fileType);
-
+    uppy.on("upload-success", (file, response) => {
+      console.log("Upload successful:", response);
       handleUploadSuccess(file, response);
+    });
+
+    uppy.on("upload-error", (file, error) => {
+      console.error("Upload error:", error);
+      setLoading(false);
     });
 
     uppyRef.current = uppy;
 
+    // uppy.getFiles().forEach(file => {
+    // uppy.setFileMeta(file.id, {
+    //   hyperlink: metadataFields.hyperlink,
+    //   additionalField: metadataFields.additionalField,
+    // })
+    // })
+
     return () => {
       uppy.close;
     };
-  }, [metadataFields]);
+  }, []);
 
   const handleUploadSuccess = async (file, response) => {
     try {
-      // Extract file data from response if available
-      const fileData = file.data || response.body || response; // Adjust as needed
-      console.log('File data to be sent:', fileData);
+      const fileData = file.data || response.body || response;
+      console.log("File data to be sent:", fileData);
 
-      // Send file to /meta endpoint
-      const metaResponse = await axios.put('http://localhost:5173/tika/tika', fileData, {
-        headers: {
-          'Content-Type': file.type,
-          'Accept': 'application/json',
-        },
-        onUploadProgress: progressEvent => {
-          const progress = (progressEvent.loaded / progressEvent.total) * 100;
-          console.log(`Meta upload progress: ${progress}%`);
+      const metaResponse = await axios.put(
+        "http://localhost:5173/tika/tika",
+        fileData,
+        {
+          headers: {
+            "Content-Type": file.type,
+            Accept: "application/json",
+          },
+          onUploadProgress: (progressEvent) => {
+            const progress = (progressEvent.loaded / progressEvent.total) * 100;
+            console.log(`Meta upload progress: ${progress}%`);
+          },
         }
-      });
-      console.log('Meta response:', metaResponse);
+      );
+      console.log("Meta response:", metaResponse);
 
       const metadata = metaResponse.data;
-      console.log('Metadata:', metadata);
+      console.log("Metadata:", metadata);
 
-      // Send file to /tika endpoint for cleaned content
-      const cleanedContentResponse = await axios.put('http://localhost:5173/tika/tika', fileData, {
-        headers: {
-          'Content-Type': file.type,
-          'Accept': 'text/plain',
-        },
-        onUploadProgress: progressEvent => {
-          const progress = (progressEvent.loaded / progressEvent.total) * 100;
-          console.log(`Content upload progress: ${progress}%`);
+      const cleanedContentResponse = await axios.put(
+        "http://localhost:5173/tika/tika",
+        fileData,
+        {
+          headers: {
+            "Content-Type": file.type,
+            Accept: "text/plain",
+          },
+          onUploadProgress: (progressEvent) => {
+            const progress = (progressEvent.loaded / progressEvent.total) * 100;
+            console.log(`Content upload progress: ${progress}%`);
+          },
         }
-      });
-      console.log('Cleaned content response:', cleanedContentResponse);
+      );
+      console.log("Cleaned content response:", cleanedContentResponse);
 
       const cleanedContent = cleanedContentResponse.data;
-      console.log('Cleaned content:', cleanedContent);
+      console.log("Cleaned content:", cleanedContent);
 
-      // Combine metadata and cleaned content
       const combinedData = {
         ...metadata,
-        'X-TIKA:content': cleanedContent, // Replacing original X-TIKA:content
-        'id': uuidv4(),
-        'hyperlink': metadataFields.hyperlink, // Use metadata from state
-        'additionalField': metadataFields.additionalField // Use metadata from state
+        "X-TIKA:content": cleanedContent,
+        id: uuidv4(),
+        hyperlink: file.meta.hyperlink || metadataFields.hyperlink,
+        additionalField:
+          file.meta.additionalField || metadataFields.additionalField,
       };
 
-      console.log('Combined data to be sent to Meilisearch:', combinedData);
+      console.log("Combined data to be sent to Meilisearch:", combinedData);
+      console.log("metadataFields:", metadataFields);
 
-      // Send combined data to Meilisearch
-      await axios.post('https://meilisearch-test.apps.silver.devops.gov.bc.ca/indexes/uppy/documents', combinedData);
+      await axios.post(
+        "https://meilisearch-test.apps.silver.devops.gov.bc.ca/indexes/uppy/documents",
+        combinedData
+      );
 
-      console.log('Original response from Tika:', metadata);
-      console.log('Cleaned content:', cleanedContent);
-      console.log('Combined data sent to Meilisearch:', combinedData);
-
-      // Handle success (e.g., notify the user)
-      alert('Document uploaded and indexed successfully!');
+      alert("Document uploaded and indexed successfully!");
     } catch (error) {
-      console.error('Error handling upload response:', error);
-      alert('There was an error uploading or indexing the document.');
+      console.error("Error handling upload response:", error);
+      alert("There was an error uploading or indexing the document.");
     } finally {
       setLoading(false); // Hide loading when upload completes or fails
     }
@@ -141,15 +156,24 @@ const UploadPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted, starting upload');
+    console.log("Form submitted, starting upload");
+    let file = uppyRef.current.getFiles()[0];
+    if (file) {
+      uppyRef.current.setFileMeta(file.id, {
+        hyperlink: metadataFields.hyperlink,
+        additionalField: metadataFields.additionalField,
+      });
+    }
+    console.log("file:", file);
     uppyRef.current.upload();
   };
 
   const handleMetadataChange = (e) => {
     const { name, value } = e.target;
+    console.log("Changing metadata:", { name, value }); // Debugging line
     setMetadataFields((prevFields) => ({
       ...prevFields,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -158,7 +182,12 @@ const UploadPage = () => {
       <div className="upload-header">
         <img src={logo} alt="Logo" className="upload-header-logo" />
         <h1>Document Upload</h1>
-        <button onClick={() => window.location.href = "/"} className="upload-upload-button">Back to Search</button>
+        <button
+          onClick={() => (window.location.href = "/")}
+          className="upload-upload-button"
+        >
+          Back to Search
+        </button>
       </div>
       <form onSubmit={handleSubmit}>
         <div>
